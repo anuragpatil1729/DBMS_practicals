88859dab6df75cf9a3b37fd0791e7a85044b3bc3
Anurag Patil <anurag151006@gmail.com>
2026-05-10 10:40:14 +0530

    Rename StudentRecordApp to jdbc_code

--- jdbc_code.java
+++ jdbc_code.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class StudentRecordApp {
+public class jdbc_code {
     public static void main(String[] args) {
 
         // Database connection details
